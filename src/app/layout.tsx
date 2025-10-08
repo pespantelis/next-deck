39c2e181476c5dfc64264cf4b5032d302fb9a83e
Pantelis Peslis <pespantelis@gmail.com>
2025-10-08 03:15:09 +0300

    Add a toggle theme button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 
 import { ProjectSidebar } from "@/components/sidebar"
+import { ThemeToggle } from "@/components/theme-toggle"
 import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
@@ -9,6 +10,7 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 import { QueryProvider } from "@/providers/query"
+import { ThemeProvider } from "@/providers/theme"
 
 import "./globals.css"
 
@@ -33,36 +35,46 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryProvider>
-          <SidebarProvider
-            style={
-              {
-                "--sidebar-width": "calc(var(--spacing) * 72)",
-                "--header-height": "calc(var(--spacing) * 12)",
-              } as React.CSSProperties
-            }
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
           >
-            <div className="flex min-h-screen w-full">
-              <ProjectSidebar />
-              <SidebarInset>
-                <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-                  <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-                    <SidebarTrigger className="-ml-1" />
-                    <Separator
-                      orientation="vertical"
-                      className="mx-2 data-[orientation=vertical]:h-4"
-                    />
-                    <h1 className="text-base font-medium">Dashboard</h1>
-                  </div>
-                </header>
-                {children}
-              </SidebarInset>
-            </div>
-          </SidebarProvider>
+            <SidebarProvider
+              style={
+                {
+                  "--sidebar-width": "calc(var(--spacing) * 72)",
+                  "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+              }
+            >
+              <div className="flex min-h-screen w-full">
+                <ProjectSidebar />
+                <SidebarInset>
+                  <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+                    <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+                      <SidebarTrigger className="-ml-1" />
+                      <Separator
+                        orientation="vertical"
+                        className="mx-2 data-[orientation=vertical]:h-4"
+                      />
+                      <h1 className="text-base font-medium">Dashboard</h1>
+                      <div className="ml-auto">
+                        <ThemeToggle />
+                      </div>
+                    </div>
+                  </header>
+                  {children}
+                </SidebarInset>
+              </div>
+            </SidebarProvider>
+          </ThemeProvider>
         </QueryProvider>
       </body>
     </html>
